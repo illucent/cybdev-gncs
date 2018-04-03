c2ee514fa1abc0a1a02b0451c47ba81f89086439
Matthias Jordan <matthias.k.jordan@gmail.com>
2018-04-03 13:56:35 -0700

    Netlify build config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,17 @@
 const contentfulConfig = require('./.contentful')
 
+// If the contentfulConfig can't be found assume the site is being built via Netlify with production environment variables
+try {
+  var contentfulConfig = require('./.contentful')
+} catch (e) {
+  var contentfulConfig = {
+    production: {
+      spaceId: process.env.SPACE_ID,
+      accessToken: process.env.ACCESS_TOKEN,
+    },
+  }
+}
+
 module.exports = {
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
@@ -7,9 +19,10 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-contentful',
-      options: process.env.NODE_ENV === 'development' ?
-        contentfulConfig.development :
-        contentfulConfig.production
+      options:
+        process.env.NODE_ENV === 'development'
+          ? contentfulConfig.development
+          : contentfulConfig.production,
     },
   ],
 }
